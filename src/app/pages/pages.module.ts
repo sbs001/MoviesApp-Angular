061b0f9d2048a5d25c43cbf1301eda2beb71ef38
Sebastian <tassone_af@yahoo.com>
2021-08-16 20:22:51 -0300

    add: pages

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
+import { SearchComponent } from './search/search.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    MovieComponent,
+    SearchComponent
+  ],
   imports: [
     CommonModule
   ]
