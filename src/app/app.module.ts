4aa0bd69ab701f83dd153e8d2c96507a8cbdae02
Sebastian <tassone_af@yahoo.com>
2021-08-17 16:35:49 -0300

    feat: add poster pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { PagesModule } from './pages/pages.module';
+import { PosterPipe } from './pipes/poster.pipe';
+import { PipesModule } from './pipes/pipes.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { PagesModule } from './pages/pages.module';
     AppRoutingModule,
     HttpClientModule,
     ComponentsModule,
-    PagesModule
+    PipesModule,
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pipes/poster.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'poster',
+})
+export class PosterPipe implements PipeTransform {
+  transform(poster: string): string {
+    return poster != null
+      ? 'https://image.tmdb.org/t/p/w500' + poster
+      : './assets/no-image.jpg';
+  }
+}
